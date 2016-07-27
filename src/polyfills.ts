1873d989e6b2c90b3254b4095bd584b2ef399b6f
Dominik Schöni <dominik@dominikschoeni.ch>
2016-07-27 11:07:09 +0200

    adding specific locale to intl-polyfill

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -4,6 +4,8 @@ import { environment } from './app/environment';
 
 // Polyfill the ECMA-402 Intl API
 import 'intl';
+import 'intl/locale-data/jsonp/en.js';
+import 'intl/locale-data/jsonp/de.js';
 
 require('zone.js/dist/zone');
 
