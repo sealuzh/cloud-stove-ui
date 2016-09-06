d945e38777465e4dcbbbe49aa1f2beb42b16bdc4
Genc Mazlami <genc.mazlami@uzh.ch>
2016-09-06 21:49:02 +0200

    Protect routes that should not by accessed without login.
    Use the canActivate method for guarding routes, according to: https://angular.io/docs/ts/latest/guide/router.html#!#can-activate-guard

--- src/app/auth/auth.routes.ts
+++ src/app/auth/auth.routes.ts
@@ -2,9 +2,10 @@ import { RouterConfig } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { UserComponent } from './user/user.component';
+import { AuthGuard } from './auth.guard'
 
 export const authRoutes: RouterConfig = [
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
-    { path: 'user', component: UserComponent }
+    { path: 'user', component: UserComponent, canActivate: [AuthGuard] }
 ];
