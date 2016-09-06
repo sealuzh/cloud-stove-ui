cc1a1e76e7cf60f8bf2ae972298f9452a1c810e6
Dominik Schöni <dominik@dominikschoeni.ch>
2016-09-07 00:15:51 +0200

    fixing some ui-stuff

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,9 @@ import { provideRouter, RouterConfig } from '@angular/router';
 
 // Components
 import {LoginComponent} from './auth/login/login.component';
+import {WelcomeComponent} from './welcome/welcome.component';
 
+// Routes
 import {applicationRoutes} from './applications/application.routes';
 import {ingredientRoutes} from './ingredients/ingredient.routes';
 import {recommendationRoutes} from './recommendations/recommendation.routes';
@@ -17,7 +19,9 @@ const routes: RouterConfig = [
   ...templateRoutes,
   ...authRoutes,
 
-  // Welcome
+  {path: 'welcome', component: WelcomeComponent},
+
+  // Default
   {path: '**', component: LoginComponent},
 
 ];
