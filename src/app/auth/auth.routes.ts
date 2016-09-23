ac704ae4789a261687609239f8823e618a72ee32
Genc Mazlami <genc.mazlami@uzh.ch>
2016-09-23 11:55:45 +0200

    Enable password change for users.

--- src/app/auth/auth.routes.ts
+++ src/app/auth/auth.routes.ts
@@ -1,11 +1,13 @@
 import { RouterConfig } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { ChangePasswordComponent } from './user/change-password.component';
 import { UserComponent } from './user/user.component';
 import { AuthGuard } from './auth.guard'
 
 export const authRoutes: RouterConfig = [
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
+    { path: 'password', component: ChangePasswordComponent },
     { path: 'user', component: UserComponent, canActivate: [AuthGuard] }
 ];
