33f260240a2956e9dfd22284c7826fc5a62f7956
Genc Mazlami <genc.mazlami@uzh.ch>
2016-09-23 16:36:47 +0200

    Add login button on feedback view.

--- src/app/auth/user/follow-up.component.ts
+++ src/app/auth/user/follow-up.component.ts
@@ -1,8 +1,9 @@
 import {Component} from '@angular/core';
-
+import {ROUTER_DIRECTIVES} from '@angular/router';
 @Component({
     template: require('./follow-up.component.html'),
     styles: [require('./follow-up.component.less')],
+    directives: [ROUTER_DIRECTIVES],
 })
 
 export class FollowUpComponent {
