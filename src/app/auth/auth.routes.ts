d0dbce00d690b80288370cfd5de030c2033978ee
Genc Mazlami <genc.mazlami@uzh.ch>
2016-09-23 12:33:45 +0200

    Add goodbye feedback page on logout/delete.

--- src/app/auth/auth.routes.ts
+++ src/app/auth/auth.routes.ts
@@ -2,6 +2,7 @@ import { RouterConfig } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ChangePasswordComponent } from './user/change-password.component';
+import { FollowUpComponent } from './user/follow-up.component';
 import { UserComponent } from './user/user.component';
 import { AuthGuard } from './auth.guard'
 
@@ -9,5 +10,6 @@ export const authRoutes: RouterConfig = [
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: 'password', component: ChangePasswordComponent },
+    { path: 'goodbye', component: FollowUpComponent },
     { path: 'user', component: UserComponent, canActivate: [AuthGuard] }
 ];
