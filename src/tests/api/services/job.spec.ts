e7b3cac69e8a710126073daf93c2f49be4771990
Dominik Schöni <dominik@dominikschoeni.ch>
2016-10-16 22:26:22 +0200

    updating tests to angular 2.0 final

---
+++ src/tests/api/services/job.spec.ts
@@ -0,0 +1,51 @@
+import { TestBed, inject } from '@angular/core/testing';
+import { MockBackend } from '@angular/http/testing';
+import { } from 'jasmine';
+
+import { BaseRequestOptions, Http, Response, ResponseOptions } from '@angular/http';
+
+import { ApiModule } from '../../../app/api/api.module';
+import { JobService } from '../../../app/api/services/job.service';
+
+describe('API: JobService', () => {
+
+    let mockBackend: MockBackend;
+    let mockJobs = [{ id: 'asdf-1337' }, { id: 'asdf-1337' }];
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [ApiModule],
+            providers: [
+                MockBackend,
+                BaseRequestOptions,
+                {
+                    provide: Http,
+                    useFactory: (backend, options) => new Http(backend, options),
+                    deps: [MockBackend, BaseRequestOptions]
+                }
+            ]
+        });
+    });
+
+    beforeEach(inject([MockBackend], function(_mockBackend: MockBackend) {
+        mockBackend = _mockBackend;
+    }));
+
+    it('should /GET a list of jobs', inject([JobService], (service: JobService) => {
+
+        let response = mockJobs;
+
+        mockBackend.connections.subscribe(connection => {
+            expect(connection.request.url).toContain('/jobs');
+            connection.mockRespond(new Response(new ResponseOptions({ status: 200, body: JSON.stringify(response) })));
+        });
+
+        service.query().subscribe(constraints => {
+            expect(constraints.length).toBe(2);
+            expect(constraints).toContain(response[0]);
+            expect(constraints).toContain(response[1]);
+        });
+
+    }));
+
+});
---
+++ src/tests/api/services/recommendation.spec.ts
@@ -0,0 +1,59 @@
+import { TestBed, inject } from '@angular/core/testing';
+import { MockBackend } from '@angular/http/testing';
+import { } from 'jasmine';
+
+import { BaseRequestOptions, Http, Response, ResponseOptions } from '@angular/http';
+
+import { ApiModule } from '../../../app/api/api.module';
+import { RecommendationService } from '../../../app/api/services/recommendation.service';
+
+describe('API: RecommendationService', () => {
+
+    let mockBackend: MockBackend;
+    let mockTrigger = { job_id: 'asdf-1337' };
+    let mockJob = { delayed_job: { attempts: 1 } };
+    let mockRecommendations = [{ id: 1 }, { id: 1 }];
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [ApiModule],
+            providers: [
+                MockBackend,
+                BaseRequestOptions,
+                {
+                    provide: Http,
+                    useFactory: (backend, options) => new Http(backend, options),
+                    deps: [MockBackend, BaseRequestOptions]
+                }
+            ]
+        });
+    });
+
+    beforeEach(inject([MockBackend], function(_mockBackend: MockBackend) {
+        mockBackend = _mockBackend;
+    }));
+
+    /**
+    * /GET constraints
+    */
+    it('should /GET a list of jobs', inject([RecommendationService], (service: RecommendationService) => {
+
+        mockBackend.connections.subscribe(connection => {
+            if (connection.request.url.indexOf('trigger_recommendation') !== -1) {
+                connection.mockRespond(new Response(new ResponseOptions({ status: 201, body: JSON.stringify(mockTrigger) })));
+            } else if (connection.request.url.indexOf('jobs/asdf-1337') !== -1) {
+                connection.mockRespond(new Response(new ResponseOptions({ status: 200, body: JSON.stringify(mockJob) })));
+            } else {
+                connection.mockRespond(new Response(new ResponseOptions({ status: 200, body: JSON.stringify(mockRecommendations) })));
+            }
+        });
+
+        service.get(1).subscribe(recommendations => {
+            expect(recommendations.length).toBe(2);
+            expect(recommendations).toContain(mockRecommendations[0]);
+            expect(recommendations).toContain(mockRecommendations[1]);
+        });
+
+    }));
+
+});
