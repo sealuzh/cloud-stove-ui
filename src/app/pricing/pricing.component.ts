33868f2dcfb86e9024282c66f31e62edf18bfc4b
Dominik Schöni <dominik@dominikschoeni.ch>
2016-11-29 17:11:14 +0100

    grouping of identical resources

--- src/app/pricing/pricing.component.ts
+++ src/app/pricing/pricing.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ResourceService } from '../api/services/resource.service';
 import { Resource } from '../api/dtos/resource.dto';
+let groupBy = require('lodash.groupby');
 
 @Component({
     template: require('./pricing.component.html'),
@@ -21,10 +22,9 @@ export class PricingComponent implements OnInit {
         {title: 'Provider', name: 'provider'},
         {title: 'Cores (#)', name: 'cores'},
         {title: 'Memory (GB)', name: 'mem_gb'},
-        {title: 'Price per Hour ($)', name: 'price_per_hour'},
-        {title: 'Price per Month ($)', name: 'price_per_month'},
-        {title: 'Price per vCPU ($)', name: 'price_per_vcpu'},
-        {title: 'Price per GB of RAM ($)', name: 'price_per_ram_gb'},
+        {title: 'Price per Hour ($)', name: 'price_per_hour_range'},
+        {title: 'Price per vCPU per Hour ($)', name: 'price_per_vcpu_range'},
+        {title: 'Price per GB of RAM per Hour ($)', name: 'price_per_ram_gb_range'},
     ];
 
     public config: any = {
@@ -44,22 +44,53 @@ export class PricingComponent implements OnInit {
 
     loadResources() {
         this._resourceService.query().subscribe(result => {
-            this.resources = result;
+            this.resources = [];
+            let groupedByRegion: any[] = groupBy(result, 'region_area');
+
+            for (let regionKey in groupedByRegion) {
+                if (groupedByRegion.hasOwnProperty(regionKey)) {
+                    let groupedByName: any[] = groupBy(groupedByRegion[regionKey], 'name');
+                    for (let resourceKey in groupedByName) {
+                        if (groupedByName.hasOwnProperty(resourceKey)) {
+                            let array = groupedByName[resourceKey];
+                            let resource: any = {};
+
+                            resource.price_per_hour_range =
+                                this.round(Math.min.apply(this, array.map(item => item.price_per_hour)))
+                                + ' - ' +
+                                this.round(Math.max.apply(this, array.map(item => item.price_per_hour)));
+
+                            resource.price_per_vcpu_range =
+                                this.round(Math.min.apply(this, array.map(item => item.price_per_hour / item.cores)))
+                                + ' - ' +
+                                this.round(Math.max.apply(this, array.map(item => item.price_per_hour / item.cores)));
+
+                            resource.price_per_ram_gb_range =
+                                this.round(Math.min.apply(this, array.map(item => item.price_per_hour / item.mem_gb)))
+                                + ' - ' +
+                                this.round(Math.max.apply(this, array.map(item => item.price_per_hour / item.mem_gb)));
+
+                            Object.assign(resource, array[0]);
+                            this.resources.push(resource);
+                        }
+                    }
+                }
+            }
+
             this.providers = this.resources.map(item => item.provider);
             this.providers = this.providers.filter((item, pos) => this.providers.indexOf(item) === pos);
 
             this.regions = this.resources.map(item => item.region_area);
             this.regions = this.regions.filter((item, pos) => this.regions.indexOf(item) === pos);
 
-            for (let resource of this.resources){
-                resource.price_per_vcpu = Math.round(resource.price_per_hour / resource.cores * 1000) / 1000;
-                resource.price_per_ram_gb = Math.round(resource.price_per_hour / resource.mem_gb * 1000) / 1000;
-            }
-
             this.onChangeTable();
         }, error => {
 
         });
+    }
+
+    round(number: number) {
+        return Math.round(number * 1000) / 1000;
     }
 
     setRegion(region: string) {
